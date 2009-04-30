bb36d5b5de013174634df201524935ec3a91d0e0
Matt Wise <mwise@slimdevices.com>
2009-04-30 19:19:14 +0000

    Make clean.sh only remove machine specific files...
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.3/trunk/squeezeos@5564 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- poky/parabuild/clean.sh
+++ poky/parabuild/clean.sh
@@ -3,10 +3,19 @@
 echo "Setting environment variables..."
 source poky-init-build-env
 
+# Check if a machine type exists
+if [ "x${MACHINE}" != "x" ]
+then
+        echo "*** Building for MACHINE type: ${MACHINE}"
+else
+        echo "ERROR: No MACHINE specified!"
+        exit 1
+fi
+
 echo "Cleaning out squeezeos-image and squeezeplay"
 bitbake squeezeos-image -c clean
 bitbake squeezeplay -c clean
 
 echo "Removing old binaries"
-rm -rf $PARABUILD_BUILD_DIR/build/tmp/deploy/images/*.bin
-rm -rf $PARABUILD_BUILD_DIR/build/tmp/deploy/images/squeezeos-image-*
+rm -rf $PARABUILD_BUILD_DIR/build/tmp/deploy/images/${MACHINE}*bin
+rm -rf $PARABUILD_BUILD_DIR/build/tmp/deploy/images/squeezeos-image-${MACHINE}*
