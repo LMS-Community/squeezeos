43057e780150f88dafa1be2b54451eeca719becf
Matt Wise <mwise@slimdevices.com>
2010-01-04 23:53:52 +0000

    Removed the system-wide poky cache download directory because it creates disk space utilization issues.
    git-svn-id: https://svn.slimdevices.com/repos/jive/7.5/trunk/squeezeos@8278 bbe22326-0783-4b3a-ac2b-7ab96b24c8d9

--- poky/parabuild/build.sh
+++ poky/parabuild/build.sh
@@ -60,7 +60,7 @@ echo "INHERIT += \"rm_work\"" >> conf/local.conf
 
 # Make build use all available CPUs
 echo "BB_NUMBER_THREADS = $CPUCOUNT
-DL_DIR = $HOME/.poky_dl_dir
+# DL_DIR = $HOME/.poky_dl_dir - commented out because it creates dangerous disk space situations
 PARALLEL_MAKE = \"-j $CPUCOUNT\" " >> conf/local.conf
 
 # Build firmware images, multiple machines can be built here
